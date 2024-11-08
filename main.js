f2dc05b1723555037cc5eac0dd05d0f775a42ed5
quincecreative <162695957+quincecreative@users.noreply.github.com>
2024-11-08 09:02:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ const createScene = () => {
   camera.wheelDeltaPercentage = 0.01;
   camera.position = new BABYLON.Vector3(0.19522278690950212, 0.32460103474098906, 2.34558162546303);
   scene.activeCamera.panningSensibility = 3000;
-  camera.pinchPrecision = 150;
+  camera.pinchPrecision = 500;
   camera.minZ = 0;
 
   camera.lowerRadiusLimit = 1;
